a63b9e7c63bbd53a46edf53a4a920ba39bf91619
Riley <rileys1000@gmail.com>
2023-05-08 17:34:31 +1200

    Refactoring, Refoldering+ Added Website Stuff

--- README.md
+++ README.md
@@ -12,3 +12,22 @@
 This README.md is so people like me and you can remember what the fuck this whole thing means when we move onto another project and forget about all of this.
 
 Ummmmmm. I started using ur code but got rid of alot of it lol. Python for life 😎
+
+# CSV to Web
+
+Turning the csv generated from the process before (firefox bookmarks to csv and nighttab bookmarks to csv) and using those combined csv files as data to flow into a webpage as their bookmarks under their categories.
+
+For the website I want a focus to be on integrating the python script into it and being able to edit how folders look and interact. With a focus on the data handling and how users can import more data if they wish and formatting data themselves as they please.
+
+## Chosen API's / extra code
+
+- Read CSV data with JavaScript/TypeScript and store in an array/object
+- Create HTML elements dynamically for each bookmark with a loop
+- Use a third-party API like RealFaviconGenerator to generate a favicon for each bookmark
+- https://github.com/RealFaviconGenerator/rfg-api
+- https://realfavicongenerator.net/
+
+---
+
+- Use a consistent color scheme and font
+- CSS transitions and hover effects
--- main.py
+++ main.py
@@ -1,67 +1,35 @@
+#!/usr/bin/env python3
 
-##
+                        # This is a script designed to pull rip and reorganise and reformat html,
+                        # json and other wierd configs of bookmarks, turn them into csv format and 
+                        # use that csv in a local hoseted webpage and use that webpage as a quicklink 
+                        # bookmarks new tab.
 
 ## Imports
-
-##
-
 import csv
 from bs4 import BeautifulSoup
 
-##
+## Config Parsing Bullshit
+import configparser
+config = configparser.ConfigParser()
+config.sections()
 
-## Constants
+## EXTERNAL PYTHON FILES
+import src.components.nighttabjson_to_csv as nt_to_csv
+import src.components.browser_bookmarks_to_csv as bm_to_csv
 
-##
+## Constants will be in the config.conf file so use that please !!!
+config.read('src\config.conf')
 
-input_bookmark_file = 'bookmarks.html'
-output_bookmark_file = 'bookmarks'
+config['DEFAULT']['BmToCsv']
+config['DEFAULT']['NtToCsv']
 
+input()
 
+## Firefox / Google Bookmarks - input file full, output folder location (will override and replace!)
+# bm_to_csv.parse(f'{config['DEFAULT']['UneditedFolder']}',config['DEFAULT']['BmToCsv'])
 
-##
-
-## Firefox / Google Bookmarks
-
-##
-
-
-with open(input_bookmark_file, 'r', encoding='utf-8') as f:
-    soup = BeautifulSoup(f, 'html.parser')
-
-# Find all divs with article information
-header_divs = soup.find_all(['h3', 'a'])
-
-# Open a new CSV file to write the data to
-with open(output_bookmark_file+'.csv', mode='w', newline='', encoding='utf-8') as file:
-    writer = csv.writer(file)
-
-    # Write the header row
-    writer.writerow(['Title', 'Link', 'Depth'])
-
-    # Loop through each div and extract the information
-    for div in header_divs:
-        # Extract the title from the h3 tag or a tag
-        if div.name == 'h3':
-            title = div.text.strip()
-            depth = len(div.find_parents(['dl', 'ul'])) + 1
-        else:
-            title = div.text.strip()
-            depth = len(div.find_parents(['dl', 'ul', 'ol'])) + 1
-
-        # Extract the link and href attribute from the a tag
-        if div.name == 'a':
-            link = div['href']
-        else:
-            link = ''
-
-        # Write the data to the CSV file
-        writer.writerow([title, link, depth])
-
-
-##
+# FIX FIX FIX FIX FIX FIX FIX!!!!!!!
 
 ## NightTab Stuff...
-
-##
-
+nt_to_csv.parse()
